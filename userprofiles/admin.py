fa714aae24806f8ca6137347eb980a31e89bef18
phonnz <agonzalez@nyxtechnology.com>
2015-12-08 15:26:32 -0600

    adding custom user model to admin

--- userprofiles/admin.py
+++ userprofiles/admin.py
@@ -1,3 +1,20 @@
+# -*- coding: utf-8 -*-
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .models import User
+
+
+class UserAdmin(UserAdmin):
+	change_list_template = "admin/change_list_filter_sidebar.html"
+	list_display = ['username', 'email', 'first_name', 'last_name', 'last_login', 'date_joined', 'is_staff','is_active' ]
+	list_editable = ('is_active',)
+	fieldsets=(
+		('Usuario', {'fields':('username', 'email', 'password' )}),
+		('Datos personales', {'fields':('first_name', 'last_name', 'mom_last_name' )}),
+		('Permisos', {'fields':('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+		)
+	list_filter = ('date_joined','is_active', 'last_login', 'is_staff')
+	search_fields = ('username', 'email')
+
+admin.site.register(User, UserAdmin)
