d2844869832e71db7005cd2854f4af44d535b5fe
phonnz <agonzalez@nyxtechnology.com>
2015-12-08 16:06:41 -0600

    adding django admin model

--- django_userprofiles/urls.py
+++ django_userprofiles/urls.py
@@ -7,6 +7,6 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'django_userprofiles.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^grappelli/', include('grappelli.urls')), # grappelli URLS
     url(r'^admin/', include(admin.site.urls)),
 )
