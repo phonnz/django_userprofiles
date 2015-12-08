2a7a8e909dbee628a9da1789e40c0f4c9de6b7be
phonnz <agonzalez@nyxtechnology.com>
2015-12-08 15:24:45 -0600

    syncdb

--- django_userprofiles/settings.py
+++ django_userprofiles/settings.py
@@ -57,10 +57,14 @@ WSGI_APPLICATION = 'django_userprofiles.wsgi.application'
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+  'default': {
+      'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+      'NAME': 'userprofiles',                      # Or path to database file if using sqlite3.
+      # The following settings are not used with sqlite3:
+      'USER': 'root',
+      'PASSWORD': 'adminadmin',
+      'HOST': 'localhost',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+  }
 }
 
 AUTH_USER_MODEL = 'userprofiles.User'
